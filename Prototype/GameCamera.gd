aa6da0e7d4eebce082de285ae42764ab01f94ede
nwallenfang <nils@wallenfang.de>
2024-01-11 12:04:04 +0100

    camera rotation

--- Prototype/GameCamera.gd
+++ Prototype/GameCamera.gd
@@ -1,12 +1,20 @@
 extends Node3D
 
 var velocity := Vector3.ZERO
-@export var acceleration := 30.0
+var rotation_input := 0.0
+@export var move_acceleration := 30.0
+## 45 degrees per second
+@export var rotation_velocity := 100.0
 @export var damping := .02
+@export var rot_damping := 0.02
 
 func _physics_process(delta: float) -> void:
-	velocity += delta * acceleration * Vector3.RIGHT * (Input.get_action_strength("move_camera_right") - Input.get_action_strength("move_camera_left"))
-	velocity += delta * acceleration * Vector3.FORWARD * (Input.get_action_strength("move_camera_forwards") - Input.get_action_strength("move_camera_backwards"))
+	# x-z movement
+	velocity += delta * move_acceleration * (basis * Vector3.RIGHT) * (Input.get_action_strength("move_camera_right") - Input.get_action_strength("move_camera_left"))
+	velocity += delta * move_acceleration * (basis * Vector3.FORWARD) * (Input.get_action_strength("move_camera_forwards") - Input.get_action_strength("move_camera_backwards"))
 	velocity *= pow(damping, delta)
-	
 	global_position += velocity * delta
+		
+	# rotation
+	rotation_input = delta * rotation_velocity * (Input.get_action_strength("rotate_camera_clockwise") - Input.get_action_strength("rotate_camera_anticlockwise"))
+	rotate_y(deg_to_rad(rotation_input))
--- Prototype/HexGrid.gd
+++ Prototype/HexGrid.gd
@@ -5,42 +5,55 @@ class_name TileGrid extends Node3D
 ## Or loading a resource defining a map.
 ## Ideally, this would even work in a tool script so in-editor as well
 
-## We're using (q,r)-axial coordinates, for a reference see
+## We're using (r,q)-axial coordinates, for a reference see
 ## https://www.redblobgames.com/grids/hexagons/#coordinates-axial
 
 
-## 2D Array of tiles, indexed with (r,q), think row, qolumn hehe :)
+## 2D Array of tiles, indexed with (r,q)
 var tiles: Array[Array] = []
-var tile_size := 1.0
 
 
 func _ready() -> void:
 	init_basic_grid(3)
 	
-
-var TileScene = preload("res://Prototype/Tile.tscn")
+var TileScene = preload("res://Logic/Tiles/Tile.tscn")
 const Q_BASIS: Vector2 = Vector2(sqrt(3), 0)
 const R_BASIS: Vector2 = Vector2(sqrt(3)/2, 3./2)
 func init_basic_grid(n: int):
 	# The origin tile will have coordinates (r=n, q=n).
 	# This makes it so the top-left tile will have coordinates 0,0 in the tiles 2D array
 	# The origin tile will be centered at the position of the TileGrid node.
-
 	# initiate 2d Array, it will have dimensions (2n, 2n)
-	for _i in range(2*n + 1):
+	for i in range(2*n + 1):
 		tiles.append([])
+		for j in range(2*n+1):
+			tiles[i].append(null)
 
 	for r in range(0, 2*n + 1):
 		for q in range(0, 2*n + 1):
 			# first off, check if this tile would have distance > n from origin
 			# in that case continue and put null into the array position
-			if false:  # TODO dist > n
-				tiles[r][q] = null 
+			if tile_distance(r, q, n, n) > n: 
 				continue
 			# place all tiles in pointy-up fashion
 			# that mean they are higher than they are wide
 			var new_tile: Tile = TileScene.instantiate()
 			add_child(new_tile)
-			var xz_translation: Vector2 = r * tile_size * Q_BASIS + q * tile_size * R_BASIS 
+			var xz_translation: Vector2 = r * Q_BASIS + q * R_BASIS 
 			new_tile.position = Vector3(xz_translation.x, 0.0, xz_translation.y)
 			new_tile.get_node("DebugLabel").text = "(%s, %s)" % [r, q]
+			tiles[r][q] = new_tile
+			
+func rq_to_tile(r: int, q: int) -> Tile:
+	return tiles[r][q]
+
+## can return null if the tile is not present
+func tile_to_rq(t: Tile) -> Vector2i:
+	return Vector2i(0, 0)
+			
+
+## could maybe be put in Utils singleton
+func tile_distance(r1: int, q1: int, r2: int, q2: int):
+	return (abs(q1 - q2) 
+			+ abs(q1 + r1 - q2 - r2)
+			+ abs(r1 - r2)) / 2
