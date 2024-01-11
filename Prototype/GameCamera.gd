ee503021471fb300608f4a5975daa8dc639eecb4
nwallenfang <nils@wallenfang.de>
2024-01-11 14:29:07 +0100

    camera zoom

--- Prototype/GameCamera.gd
+++ Prototype/GameCamera.gd
@@ -5,8 +5,17 @@ var rotation_input := 0.0
 @export var move_acceleration := 30.0
 ## 45 degrees per second
 @export var rotation_velocity := 100.0
-@export var damping := .02
-@export var rot_damping := 0.02
+@export var damping := .03
+
+
+@onready var zoom_pivot = $AnglerPivot
+var camera_zoom := 3.0
+var zoom_velocity := 0.0
+@export var zoom_factor := 5.0
+@export var zoom_damping := 0.01
+
+func _ready() -> void:
+	zoom_pivot.position.y = camera_zoom
 
 func _physics_process(delta: float) -> void:
 	# x-z movement
@@ -18,3 +27,15 @@ func _physics_process(delta: float) -> void:
 	# rotation
 	rotation_input = delta * rotation_velocity * (Input.get_action_strength("rotate_camera_clockwise") - Input.get_action_strength("rotate_camera_anticlockwise"))
 	rotate_y(deg_to_rad(rotation_input))
+	
+	# zoom
+	if Input.is_action_just_released("zoom_camera_out"):
+		zoom_velocity += delta * zoom_factor
+	elif Input.is_action_just_released("zoom_camera_in"):
+		zoom_velocity -= delta * zoom_factor
+	
+	zoom_velocity *= pow(zoom_damping, delta)
+	
+	camera_zoom += zoom_velocity
+	camera_zoom = clamp(camera_zoom, .80, 12.0)
+	zoom_pivot.position.y = camera_zoom
