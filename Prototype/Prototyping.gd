a29a77252d2556731d8d35c89d8282a916f40d46
nwallenfang <nils@wallenfang.de>
2024-01-12 20:20:47 +0100

    raycast start

--- Prototype/Prototyping.gd
+++ Prototype/Prototyping.gd
@@ -23,3 +23,19 @@ func _on_entity_find_button_pressed() -> void:
 		var movement_tiles = $TileGrid.get_all_tiles_in_distance(3, 3, 2)
 		$TileGrid.unhighlight_entity_type(ent_type)
 		flip2 = false
+
+
+
+@onready var mouse_ray = %MouseRaycast
+func _physics_process(delta: float) -> void:
+	var mouse_position := get_viewport().get_mouse_position()
+	var camera := get_viewport().get_camera_3d()
+	var ray_origin := camera.project_ray_origin(mouse_position)
+	var ray_direction := camera.project_ray_normal(mouse_position)
+	var ray_length := camera.far
+	var end := ray_origin + ray_direction * ray_length
+	mouse_ray.target_position = mouse_ray.to_local(end)
+	
+	if mouse_ray.is_colliding():
+		#print(mouse_ray.get_collider())
+		pass
