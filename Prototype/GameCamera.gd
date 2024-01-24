93299ea4005355edb93229262076b97236913864
nheeb <nitai64@yahoo.de>
2024-01-24 15:27:27 +0100

    Camera is clean

--- Prototype/GameCamera.gd
+++ Prototype/GameCamera.gd
@@ -8,7 +8,6 @@ var rotation_input_vertical := 0.0
 @export var rotation_velocity := 130.0
 @export var damping := .02
 
-
 @onready var zoom_pivot = $AnglePivot/ZoomPivot
 var camera_zoom := 3.0
 var zoom_velocity := 0.0
@@ -16,8 +15,15 @@ var zoom_velocity := 0.0
 @export var zoom_damping := 0.01
 @export var mouse_rotation_factor := .3
 
+signal target_reached
+
 var player_input_enabled := true
 var follow_target: Node3D = null
+var just_reach_target := false
+
+@export var follow_damp_ease := 8.0
+@export var follow_damp_range := 2.5
+@export var target_reach_range := .2
 
 func _ready() -> void:
 	zoom_pivot.position.y = camera_zoom
@@ -30,13 +36,26 @@ func _unhandled_input(event: InputEvent) -> void:
 			rotation_input_vertical = - event.relative.y * mouse_rotation_factor
 
 func _physics_process(delta: float) -> void:
+	delta *= VisualTime.visual_time_scale
+	
 	# x-z movement
 	if player_input_enabled:
 		velocity += delta * move_acceleration * (basis * Vector3.RIGHT) * (Input.get_action_strength("move_camera_right") - Input.get_action_strength("move_camera_left"))
 		velocity += delta * move_acceleration * (basis * Vector3.FORWARD) * (Input.get_action_strength("move_camera_forwards") - Input.get_action_strength("move_camera_backwards"))
+		
 	if follow_target != null:
+		var dist := global_position.distance_to(follow_target.global_position)
 		velocity += delta * move_acceleration * 1.6 * global_position.direction_to(follow_target.global_position)
-	velocity *= pow(damping, delta)
+		velocity *= pow(damping, delta)
+		velocity *=  (1.0 - ease(Utility.clamp_map(dist, 0.0, follow_damp_range, 1.0, 0.0), follow_damp_ease))
+		
+		if just_reach_target and dist < target_reach_range:
+			follow_target = null
+			just_reach_target = false
+			target_reached.emit()
+	else:
+		velocity *= pow(damping, delta)
+	
 	global_position += velocity * delta
 		
 	# rotation
