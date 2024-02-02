eed322bb975d31387f64011b50e93709f2d4f424
Sam Holder <samholder@live.co.uk>
2024-02-02 20:22:02 +0000

    Uncommented unload.

--- Spells/AllSpells/Unload.gd
+++ Spells/AllSpells/Unload.gd
@@ -25,10 +25,10 @@ func casting_effect() -> void:
 	var enemies = target.get_enemies()
 	assert(len(enemies) >= 1, "Unleash expects min 1 enemy on tile")
 	
-	#var hand_size = len(combat.hand) - 1
-	#for card in combat.hand:
-		#combat.cards.discard(card)
-	#
-	#for enemy in enemies:
-		#enemy.hp -= hand_size
+	var hand_size = len(combat.hand) - 1
+	for card in combat.hand:
+		combat.cards.discard(card)
+	
+	for enemy in enemies:
+		enemy.hp -= hand_size
 
