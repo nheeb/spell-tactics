4a27c2ebe70808ad36434ddafafcbbd86ea57239
Sam Holder <samholder@live.co.uk>
2024-02-02 20:21:41 +0000

    added temporary quick randomisation for level selction.

--- Logic/Overworld/OverworldMap.gd
+++ Logic/Overworld/OverworldMap.gd
@@ -8,33 +8,36 @@ func generate_prototype_layout():
 	])
 	for i in range(1):
 		node_sets.append([
-			LevelNodeData.create(LevelNode.NodeType.Battle),
-			LevelNodeData.create(LevelNode.NodeType.Battle),
-			LevelNodeData.create(LevelNode.NodeType.Battle),
+			LevelNodeData.create_battle(get_random_level()),
+			LevelNodeData.create_battle(get_random_level()),
+			LevelNodeData.create_battle(get_random_level()),
 		])
 	node_sets.append([
 		LevelNodeData.create(LevelNode.NodeType.Event),
 		LevelNodeData.create(LevelNode.NodeType.Event),
 	])
 	node_sets.append([
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
 	])
 	node_sets.append([
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
 	])
 	node_sets.append([
 		LevelNodeData.create(LevelNode.NodeType.Event),
 		LevelNodeData.create(LevelNode.NodeType.Event),
 	])
 	node_sets.append([
-		LevelNodeData.create(LevelNode.NodeType.Battle),
-		LevelNodeData.create(LevelNode.NodeType.Battle),
+		LevelNodeData.create_battle(get_random_level()),
+		LevelNodeData.create_battle(get_random_level()),
 	])
 	node_sets.append([
 		LevelNodeData.create(LevelNode.NodeType.Boss)
 	])
+
+func get_random_level():
+	return ['res://Levels/Area1/rivers.tres', 'res://Levels/Area1/clearing.tres'].pick_random()
