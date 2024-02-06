fe937c16803bf505ad9841601acb54ec54ef8a15
Sam Holder <samholder@live.co.uk>
2024-02-06 21:16:19 +0000

    merge

--- UI/PostBattle.gd
+++ UI/PostBattle.gd
@@ -1,4 +1,26 @@
 extends Control
 
+@export var booster_packs: Array[BoosterPack] = []
+
+@onready var grid: Control = $"PickBoosterPopup/VBoxContainer/GridContainer"
+
 func _on_continue_pressed():
 	Game.view_orchestrator.transition_to_overworld()
+
+func _ready():
+	var items = booster_packs.duplicate()
+	var booster_1 = items.pick_random()
+	items.remove_at(items.find(booster_1))
+	var booster_2 = items.pick_random()
+	items.remove_at(items.find(booster_2))
+	var booster_3 = items.pick_random()
+	items.remove_at(items.find(booster_3))
+
+	var boosters = [booster_1, booster_2, booster_3]
+	for booster in boosters:
+		var option = preload("res://UI/PostBattle/BoosterPickOption.tscn").instantiate()
+		option.booster_pack = booster
+		grid.add_child(option)
+
+	var choose_coin = preload("res://UI/PostBattle/CoinsPickOption.tscn").instantiate()
+	grid.add_child(choose_coin)
