5368652c16937b712ea04caa5d416ddc158f791f
nheeb <34535625+nheeb@users.noreply.github.com>
2024-02-16 17:43:53 +0100

    lightning

---
+++ Spells/AllSpells/LightningStrinke.gd
@@ -0,0 +1,32 @@
+extends SpellLogic
+
+## Usable references:
+## spell - Corresponding spell
+##   (with round_persistant_properties & combat_persistant_properties)
+## combat - The current combat for which the spell was created
+## target - The target Tile (if Spell is targetable)
+
+## The current costs with all the modifiers if there are any
+#func _get_costs() -> EnergyStack:
+	#return spell.type.costs
+
+## This is for overriding if there are general cast-conditions
+#func _is_unlocked() -> bool:
+	#return true
+
+## This is for overriding if there are conditions for targets
+#func is_current_cast_valid() -> bool:
+	#return true
+
+## Here should be the effect
+func casting_effect() -> void:
+	var base_damage := 3
+	target = target as Tile
+	for enemy in target.get_enemies():
+		enemy = enemy as EnemyEntity
+		if enemy.get_status_effect("wet"):
+			enemy.inflict_damage_with_visuals(2*base_damage)
+		else:
+			enemy.inflict_damage_with_visuals(1*base_damage)
+		
+
