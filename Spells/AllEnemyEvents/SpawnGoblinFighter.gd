5449181da1c131fd0bbc35c0457a94607211d8d1
nheeb <34535625+nheeb@users.noreply.github.com>
2024-02-16 18:16:12 +0100

    Fixed Goblin Spawn

--- Spells/AllEnemyEvents/SpawnGoblinFighter.gd
+++ Spells/AllEnemyEvents/SpawnGoblinFighter.gd
@@ -11,10 +11,15 @@ func get_event_length() -> int:
 const GOBLIN = preload("res://Entities/Enemies/Goblin.tres")
 
 func event_effect(round_number: int) -> void:
-	var location = combat.level.get_center_tile().location
-	var goblin = combat.level.entities().create_entity(location, GOBLIN, false)
-	combat.enemies.append(goblin)
-	combat.animation.camera_reach(goblin.visual_entity)
-	combat.animation.wait(.4)
-	combat.animation.show(goblin.visual_entity)
-	combat.animation.effect(VFX.HEX_RINGS, goblin.visual_entity, {"color": Color.RED})
+	for enemy in combat.get_all_enemies():
+		for tile in enemy.current_tile.get_surrounding_tiles():
+			if bool(tile.get_obstacle_layers() & GOBLIN.obstacle_mask):
+				continue
+			var location = tile.location
+			var goblin = combat.level.entities().create_entity(location, GOBLIN, false)
+			combat.enemies.append(goblin)
+			combat.animation.camera_reach(goblin.visual_entity)
+			combat.animation.wait(.4)
+			combat.animation.show(goblin.visual_entity)
+			combat.animation.effect(VFX.HEX_RINGS, goblin.visual_entity, {"color": Color.RED})
+			return
