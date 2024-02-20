4bb07d30770ad97c3b897c65ad39417c2283a529
Sam Holder <samholder@live.co.uk>
2024-02-20 22:38:11 +0000

    sync deck size label with adventure state.

--- UI/OverworldOverlay.gd
+++ UI/OverworldOverlay.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var health_label = $"./HBoxContainer2/HealthLabel"
 @onready var coins_label = $"./HBoxContainer2/CoinsLabel"
+@onready var deck_size_label = $"./ViewDeck/HBoxContainer/Label"
 
 func _ready():
 	_update()
@@ -10,12 +11,16 @@ func _ready():
 func _update():
 	_update_coins()
 	_update_health()
+	_update_deck_size()
 
 func _update_coins():
 	coins_label.text = str(Adventure.coins)
 
 func _update_health():
 	health_label.text = str(Adventure.health)
+
+func _update_deck_size():
+	deck_size_label.text = str(len(Adventure.deck_states))
 
 func _on_view_deck_pressed():
 	ActivityManager.push(BrowseDeckActivity.new())
