f035e0ea7b023e863416b551d08df71847a0d76e
Nils Wallenfang <nils@wallenfang.de>
2024-04-03 23:14:06 +0200

    dirt and grass first textures

--- Entities/Visuals/PrototypeBillboard.gd
+++ Entities/Visuals/PrototypeBillboard.gd
@@ -9,10 +9,10 @@ static func get_billboard_filepath(entity_name) -> String:
 	return Game.PROTOTYPE_BILLBOARD_DIR + entity_name + ".png"
 
 func set_texture(path: String) -> void:
-	$MeshInstance3D.material_override.set("shader_parameter/texture_albedo", load(path))
+	$Quad.material_override.set("shader_parameter/texture_albedo", load(path))
 
 func set_texture_from_entity_name(entity_name: String) -> void:
 	set_texture(PrototypeBillboard.get_billboard_filepath(entity_name))
 
 func drain_transition(drained: bool = true) -> void:
-	VisualTime.new_tween().tween_property($MeshInstance3D.material_override, "shader_parameter/drain_progress", float(drained), 1.0)
+	VisualTime.new_tween().tween_property($Quad.material_override, "shader_parameter/drain_progress", float(drained), 1.0)
