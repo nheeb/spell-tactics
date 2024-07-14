bc7743e93c665d1826d6e23a28e5f3e6e462563c
nheeb <34535625+nheeb@users.noreply.github.com>
2024-07-14 19:19:42 +0200

    Clean Plugin

--- addons/psychosis_programming/PsychScreen.gd
+++ addons/psychosis_programming/PsychScreen.gd
@@ -10,12 +10,20 @@ var plugin: EditorPlugin
 func psychosis():
 	if plugin:
 		var tab_bar = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_children()[0].get_children()[2].get_children()
-		tab_bar[4].visible = false
+		var plugin_button: Button
+		var script_button: Button
+		for c in tab_bar:
+			if "Psychosis" in str(c):
+				plugin_button = c as Button
+			elif "Script" in str(c):
+				script_button = c as Button
+		if plugin_button: plugin_button.visible = false
 		await get_tree().create_timer(.1).timeout
-		(tab_bar[2] as Button).button_down.emit()
-		(tab_bar[2] as Button).pressed.emit()
-		await get_tree().create_timer(.1).timeout
-		(tab_bar[2] as Button).button_up.emit()
+		if script_button:
+			script_button.button_down.emit()
+			script_button.pressed.emit()
+			await get_tree().create_timer(.1).timeout
+			script_button.button_up.emit()
 		plugin.psychosis()
 		plugin.main_screen = false
 		visible = false
