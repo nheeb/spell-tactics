22069e43059a9ce60cb1c1102d2034163200bfb0
Markus <markushorrmann@gmail.com>
2024-11-06 09:33:07 +0100

    new Branch for Doomfist

--- Content/Enemies/Moves/Melee/RocketPunchExecute.gd
+++ Content/Enemies/Moves/Melee/RocketPunchExecute.gd
@@ -6,6 +6,54 @@ func _execute():
 	var tiles_in_the_way := combat.level.get_line(
 		enemy.current_tile, target_entity.current_tile
 	)
+<<<<<<< HEAD
+	
+	var obstacle_tile
+	var previous_tile
+	
+	for tile in tiles_in_the_way:
+		combat.animation.effect(VFX.HEX_COLOR, tile)
+		if tile.is_obstacle():
+			tile.get_obstacle_layers()
+			obstacle_tile = tile
+			break
+		previous_tile = tile  # Update previous tile if current one is not an obstacle
+	
+	var blocking_entity: Entity = null
+	
+	if obstacle_tile:
+		for ent in obstacle_tile.entities:
+			var cover := ent.type.cover_value as int
+			if cover >= 1:
+				blocking_entity = ent
+				break
+	
+	if obstacle_tile:
+		var destination := previous_tile as Tile
+		combat.movement.move_entity(enemy, destination)
+
+		if blocking_entity:
+			blocking_entity.die()
+	elif tiles_in_the_way.size() > 1:
+		tiles_in_the_way.pop_back()
+		var destination := tiles_in_the_way[-1]
+		combat.movement.move_entity(enemy, destination)
+		(target_entity as HPEntity).inflict_damage_with_visuals(3)
+		combat.movement.apply_knockback(
+			target_entity,
+			start_tile.direction_to(target_entity.current_tile)
+		)
+
+func _is_possible(enemy_tile: Tile) -> bool:
+	
+	#var tiles_in_the_way := combat.level.get_line(
+		#enemy_tile, target_entity.current_tile
+	#)
+	#for tile in tiles_in_the_way:
+		#if tile.is_obstacle():
+			#tile.get_obstacle_layers()
+			#return false
+=======
 	if tiles_in_the_way.size() > 1:
 		tiles_in_the_way.pop_back()
 		var destination := tiles_in_the_way[-1]
@@ -23,4 +71,5 @@ func _is_possible(enemy_tile: Tile) -> bool:
 	for tile in tiles_in_the_way:
 		if tile.is_obstacle():
 			return false
+>>>>>>> main
 	return true
