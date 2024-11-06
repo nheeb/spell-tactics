7d70ad4cd5d1a5160239e929ea2fe0cb734fbae1
Markus <markushorrmann@gmail.com>
2024-11-06 09:37:12 +0100

    Fixed some remaining merge conflicts

--- Content/Enemies/Moves/Melee/RocketPunchExecute.gd
+++ Content/Enemies/Moves/Melee/RocketPunchExecute.gd
@@ -6,7 +6,6 @@ func _execute():
 	var tiles_in_the_way := combat.level.get_line(
 		enemy.current_tile, target_entity.current_tile
 	)
-<<<<<<< HEAD
 	
 	var obstacle_tile
 	var previous_tile
@@ -53,23 +52,5 @@ func _is_possible(enemy_tile: Tile) -> bool:
 		#if tile.is_obstacle():
 			#tile.get_obstacle_layers()
 			#return false
-=======
-	if tiles_in_the_way.size() > 1:
-		tiles_in_the_way.pop_back()
-		var destination := tiles_in_the_way[-1]
-		combat.movement.move_entity(enemy, destination)
-	(target_entity as HPEntity).inflict_damage_with_visuals(3)
-	combat.movement.apply_knockback(
-		target_entity,
-		start_tile.direction_to(target_entity.current_tile)
-	)
-
-func _is_possible(enemy_tile: Tile) -> bool:
-	var tiles_in_the_way := combat.level.get_line(
-		enemy_tile, target_entity.current_tile
-	)
-	for tile in tiles_in_the_way:
-		if tile.is_obstacle():
-			return false
->>>>>>> main
+			
 	return true
