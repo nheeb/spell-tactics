d693921576725a9c68e7caa9def8983cd3d2d524
nheeb <34535625+nheeb@users.noreply.github.com>
2024-12-04 18:45:03 +0100

    Fortified Visuals

--- Content/Spells/SummonPlant.gd
+++ Content/Spells/SummonPlant.gd
@@ -7,5 +7,8 @@ func execute() -> void:
 	var plant_type := plant_pool.pick_random() as EntityType
 	assert(plant_type)
 	combat.animation.effect(VFX.HEX_RINGS, target_tile, {"color": Color.YELLOW}).set_duration(1.5)
+	combat.animation.record_start()
 	var plant := plant_type.create_entity(combat, target_tile)
 	plant.apply_status(Preloaded.STATUS_FORTIFIED)
+	await combat.action_stack.wait()
+	combat.animation.record_finish_as_subqueue().set_flag_with()
